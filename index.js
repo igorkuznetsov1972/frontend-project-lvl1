7f14982f97a475c94269982d5f79ffde47f5d81b
Igor <ikk@mail.ru>
2020-04-09 19:02:24 +0300

    third commit

---
+++ index.js
@@ -0,0 +1,7 @@
+import readlineSync from 'readline-sync';
+const playerGreeting = () => {
+    console.log('Welcome to the Brain games!');
+    const playerName = readlineSync.question('May I have your name? ');
+    console.log('Hello, ' + playerName + '!');
+};
+export {playerGreeting};
