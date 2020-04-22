b29fbdb60ce4a9924169ca57701b86f728b2f506
Igor <ikk@mail.ru>
2020-04-22 13:15:18 +0300

    game core second commit

--- brain-even-core.js
+++ brain-even-core.js
@@ -1,18 +1,21 @@
 import readlineSync from 'readline-sync';
 import _ from 'lodash';
 
-const isEven = (num) => (num % 2 === 0);
-const isRightAnswer = (num, answer) => {
-  if (isEven(num) && answer === 'yes') return true;
-  if (!isEven(num) && answer === 'no') return true;
-  return false;
-};
-const numToPlayWith = () => _.random(1, 1000);
-const answer = (num) => readlineSync.question(`Question: ${num} `);
-const gamePlay = () => {
+const playEvenGame = () => {
+  const isEven = (num) => (num % 2 === 0);
+  const isRightAnswer = (num, answer) => {
+    if (isEven(num) && answer === 'yes') return true;
+    if (!isEven(num) && answer === 'no') return true;
+    return false;
+  };
+  const numToPlayWith = () => _.random(1, 1000);
+  console.log('Welcome to the Brain games!');
+  const playerName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${playerName}!`);
+  console.log('Answer "yes" if the number is even, otherwise answer "no".');
   for (let i = 0; i < 3; i += 1) {
     const num = numToPlayWith();
-    const currAnswer = answer(num);
+    const currAnswer = readlineSync.question(`Question: ${num} `);
     if (isRightAnswer(num, currAnswer)) console.log('Correct!');
     else if (currAnswer === 'yes') {
       console.log(`"yes" is wrong answer ;(. Correct answer was "no".
@@ -25,4 +28,4 @@ Let's try again, ${playerName}!`);
     }
   }
 };
-export default gamePlay;
+export default playEvenGame;
