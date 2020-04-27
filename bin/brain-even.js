46f2f282f9df18e130fd609cb585f3d30082333e
Igor <ikk@mail.ru>
2020-04-27 20:13:11 +0300

    new files structure

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,4 +1,4 @@
 #! /usr/bin/env node
-import playEvenGame from '../brain-even-core.js';
+import playEvenGame from '../games/brain-even-core.js';
 
 playEvenGame();
--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,4 +1,4 @@
 #! /usr/bin/env node
-import playerGreeting from '../index.js';
+import playerGreeting from '../src/index.js';
 
 playerGreeting();
