e6cf295f0ea7ab59229da370ad732b51736db917
Igor <ikk@mail.ru>
2020-04-28 16:17:23 +0300

    brain progression game exe core and package.json first commit

---
+++ bin/brain-progression.js
@@ -0,0 +1,4 @@
+#! /usr/bin/env node
+import playProgressionGame from '../games/brain-progression-core.js';
+
+playProgressionGame();
---
+++ games/brain-progression-core.js
@@ -0,0 +1,47 @@
+import readlineSync from 'readline-sync';
+import _ from 'lodash';
+import * as dialog from '../src/index.js';
+
+const generateProgression = () => {
+  const firstNumber = _.random(1000);
+  const incrementNumber = _.random(1000);
+  const hiddenNumberPosition = _.random(9);
+  let hiddenNumber;
+  const progression = [];
+  if (hiddenNumberPosition === 0) {
+    progression.push('..');
+    hiddenNumber = firstNumber;
+  } else progression.push(firstNumber);
+  let nextNumber = firstNumber;
+  for (let i = 1; i < 10; i += 1) {
+    nextNumber += incrementNumber;
+    if (i === hiddenNumberPosition) {
+      progression.push('..');
+      hiddenNumber = nextNumber;
+    } else progression.push(nextNumber);
+  }
+  return {
+    progression, hiddenNumber,
+  };
+};
+
+const playProgressionGame = () => {
+  const playerName = dialog.playerGreeting();
+  console.log('What number is missing in the progression?');
+  for (let i = 0; i < 3; i += 1) {
+    const {
+      progression, hiddenNumber,
+    } = generateProgression();
+    const currentAnswer = readlineSync.questionInt(`Question: ${progression}  `);
+    if (hiddenNumber === currentAnswer && i === 2) {
+      dialog.lastCorrectAnswer(playerName);
+      break;
+    }
+    if (hiddenNumber === currentAnswer) dialog.correctAnswer();
+    else {
+      dialog.wrongAnswer(currentAnswer, hiddenNumber, playerName);
+      break;
+    }
+  }
+};
+export default playProgressionGame;
