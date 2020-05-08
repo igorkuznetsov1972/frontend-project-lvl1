2f1db531774defd1f087e5ffad9f29c60314805e
Igor <ikk@mail.ru>
2020-05-08 14:22:14 +0300

    unneeded bordercase removed

--- src/games/brain-prime-core.js
+++ src/games/brain-prime-core.js
@@ -3,7 +3,6 @@ import gameFlow from '../index.js';
 
 const isPrime = (num) => {
   if (num < 2) return false;
-  if (num === 2) return true;
   const finish = Math.sqrt(num);
   for (let i = 2; i <= finish; i += 1) {
     if (num % i === 0) return false;
