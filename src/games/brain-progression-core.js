dc18390e1308d67119a15b3b58983675ea1323a4
Igor <ikk@mail.ru>
2020-05-11 15:04:26 +0300

    progression game variables naming improved

--- src/games/brain-progression-core.js
+++ src/games/brain-progression-core.js
@@ -3,20 +3,20 @@ import gameFlow from '../index.js';
 
 const gameDescription = 'What number is missing in the progression?';
 const progressionLength = 10;
-const generateProgression = (firstNumber, difference) => {
-  const progression = [firstNumber];
+const generateProgression = (firstMember, difference) => {
+  const progression = [firstMember];
   for (let position = 1; position < progressionLength; position += 1) {
-    progression.push(firstNumber + (position * difference));
+    progression.push(firstMember + (position * difference));
   }
   return progression;
 };
 const getGameData = () => {
-  const firstNumber = _.random(1000);
+  const firstMember = _.random(1000);
   const difference = _.random(1000);
-  const hiddenNumberPosition = _.random(0, progressionLength - 1);
-  const progression = generateProgression(firstNumber, difference);
-  const expectedAnswer = progression[hiddenNumberPosition].toString();
-  progression.splice(hiddenNumberPosition, 1, '..');
+  const hiddenMemberPosition = _.random(0, progressionLength - 1);
+  const progression = generateProgression(firstMember, difference);
+  const expectedAnswer = progression[hiddenMemberPosition].toString();
+  progression.splice(hiddenMemberPosition, 1, '..');
   const question = progression.join(' ');
   return { expectedAnswer, question };
 };
